ad4f4472dd3b0c4a8b39accfbd47fd91b1746170
harshalitalele <harshalitalele@gmail.com>
2016-01-26 15:16:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,36 @@ angular.module('colorPalette', [])
     }, {
         title: 'White',
         code: '#FFFFFF'
+    }, {
+        title: 'Dark Red',
+        code: '#C00000'
+    }, {
+        title: 'Red',
+        code: '#FF0000'
+    }, {
+        title: 'Orange',
+        code: '#FFC000'
+    },{
+        title: 'Yellow',
+        code: '#FFFF00'
+    }, {
+        title: 'Light Green',
+        code: '#92D050'
+    }, {
+        title: 'Green',
+        code: '#00B050'
+    }, {
+        title: 'Light Blue',
+        code: '#00B0F0'
+    }, {
+        title: 'Blue',
+        code: '#0000FF'
+    }, {
+        title: 'Dark Blue',
+        code: '#002060'
+    }, {
+        title: 'Purple',
+        code: '#7030A0'
     }];
     
     $scope.selectColor = function(selectedColor) {
